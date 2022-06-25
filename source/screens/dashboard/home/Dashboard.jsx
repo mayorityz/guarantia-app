118ed54ce4dcb39b5080b98a7dba0712234ecef0
Mayowa <mayority11@gmail.com>
2022-06-25 23:38:35 +0100

    tabs in dashbiard

--- source/screens/dashboard/home/Dashboard.jsx
+++ source/screens/dashboard/home/Dashboard.jsx
@@ -4,8 +4,11 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import { GlobalStyle } from '../../../constants/GlobalStyle'
 import { DashHomeStyle } from './Style'
 import { Fonts } from '../../../constants/Fonts'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+import { appColor } from '../../../constants/Colors'
 
 export default function Dashboard() {
+  const Tab = createMaterialTopTabNavigator()
   const actionOptions = [
     {
       title: '',
@@ -16,7 +19,15 @@ export default function Dashboard() {
   return (
     <AppWrapper>
       <View style={[GlobalStyle.p20]}>
-        <Text>Welcome back, Mayowa</Text>
+        <Text
+          style={{
+            fontSize: 19,
+            fontFamily: Fonts.Monsterat600,
+            marginTop: 15,
+          }}
+        >
+          Welcome back, Mayowa
+        </Text>
         <View style={[DashHomeStyle.WalletBalance]}>
           <Text style={[DashHomeStyle.WalletValue]}>N0.00</Text>
         </View>
@@ -43,7 +54,38 @@ export default function Dashboard() {
             </Text>
           </View>
         </TouchableOpacity>
+        <View
+          style={{
+            width: '100%',
+            height: 1.5,
+            backgroundColor: '#f6f6f6',
+            marginVertical: 15,
+          }}
+        />
+        <View style={{ height: 256 }}>
+          <Tab.Navigator
+            screenOptions={{
+              tabBarLabelStyle: {
+                fontFamily: Fonts.Monsterat500,
+                fontSize: 9,
+                color: appColor.charcoal,
+                textTransform: 'capitalize',
+              },
+            }}
+          >
+            <Tab.Screen name="Received Requests" component={S1} />
+            <Tab.Screen name="My Requests" component={S1} />
+          </Tab.Navigator>
+        </View>
       </View>
     </AppWrapper>
+  )
+}
+
+function S1() {
+  return (
+    <View style={{ backgroundColor: '#f6f6f6', height: 256, padding: 12 }}>
+      <Text>ok</Text>
+    </View>
   )
 }
